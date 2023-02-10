e91091a40b154c076b4bbba4b902483d452ecc1e
Sahil <sahilchauhan45448@gmail.com>
2023-02-10 10:41:39 +0530

    add app

---
+++ 01-birthday-reminder/src/List.js
@@ -0,0 +1,20 @@
+import React from 'react';
+const List = ({people}) => {
+  return (
+    <>
+      {people.map((person) => {
+        return (
+          <article className='person' key={person.id}>
+            <img src={person.image} alt={person.name} />
+            <div>
+              <h4>{person.name}</h4>
+              <p>{person.age} years</p>
+            </div>
+          </article>
+        )
+      })}
+    </>
+  );
+};
+
+export default List;
