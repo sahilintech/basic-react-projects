77a47a1ad88352e730c2d16c915d324a507781ab
Sahil <sahilchauhan45448@gmail.com>
2023-03-04 00:55:13 +0530

    update app

--- 11-navbar/src/Navbar.js
+++ 11-navbar/src/Navbar.js
@@ -4,17 +4,32 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  useEffect(()=>{
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if(showLinks)
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    else
+      linksContainerRef.current.style.height = "0px"
+
+  },[showLinks])
+  const handleClick = () =>{
+    setShowLinks(!showLinks)
+  }
   return (
     <nav>
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} alt="logo" />
-          <button className="nav-toggle">
+          <button className="nav-toggle" onClick={handleClick}>
             <FaBars />
           </button>
         </div>
-        <div className="links-container show-container">
-          <ul className="links">
+        <div className="links-container" ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
             {
               links.map((link) => {
                 return (
